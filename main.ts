8e06dc1401ebde688608ee4a1b315f3f3d2692d1
auto-moose <kaab2209@gmail.com>
2025-01-12 23:58:26 +0000

    Qol changes, add level 2, start adding new item

--- main.ts
+++ main.ts
@@ -2,6 +2,7 @@ namespace SpriteKind {
     export const camera = SpriteKind.create()
     export const Block = SpriteKind.create()
     export const image = SpriteKind.create()
+    export const Bomb = SpriteKind.create()
 }
 let list: number[] = []
 // States
@@ -29,14 +30,28 @@ function setupLevel () {
         blockX = 0
         blockY = 13
         minBlockY = 10
-        maxBlockY = 15
+        maxBlockY = 16
         minBlockX = 0
         maxBlockX = 7
+        coins = 5
+        itemsUnlocked = 2
+        map = tilemap`level1`
+    } else if (level == 2) {
+        playerGridX = 5
+        playerGridY = 13
+        blockX = 4
+        blockY = 13
+        minBlockY = 10
+        maxBlockY = 16
+        minBlockX = 0
+        maxBlockX = 11
         coins = 1
         itemsUnlocked = 1
+        map = tilemap`level0`
     }
 }
 function mainMenu () {
+    state = -1
     titleText.setText("")
     music.stopAllSounds()
     music.setVolume(100)
@@ -60,6 +75,17 @@ controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
         }
         grid.place(blockSelect, tiles.getTileLocation(blockX, blockY))
         updateBlockSelect()
+    } else if (state == 2) {
+        if (shopMenuSelect - 2 == 1) {
+            shopMenuSelect = 0
+            refreshShop()
+        } else if (shopMenuSelect - 2 >= 0) {
+            shopMenuSelect = shopMenuSelect - 2
+            refreshShop()
+        } else {
+            music.setVolume(255)
+            music.play(music.melodyPlayable(music.footstep), music.PlaybackMode.InBackground)
+        }
     }
 })
 function placeBlock () {
@@ -72,8 +98,11 @@ function placeBlock () {
         blockListType.push(1)
         blockListX.push(blockX)
         blockListY.push(blockY)
-    } else {
-    	
+    } else if (selectedBlock == 2) {
+        tiles.setTileAt(tiles.getTileLocation(blockX, blockY), assets.tile`bomb`)
+        blockListType.push(2)
+        blockListX.push(blockX)
+        blockListY.push(blockY)
     }
     selectedBlock = 0
     updateBlockSelect()
@@ -85,6 +114,18 @@ controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
         blockSelectMenu()
     } else if (state == 2) {
         loadLevel()
+    } else if (state == 3) {
+        scene.cameraShake(4, 200)
+        rob.setVelocity(0, 10)
+        rob.ay = 20
+        sprites.destroy(rob, effects.fire, 500)
+        music.stopAllSounds()
+        music.setVolume(255)
+        music.play(music.melodyPlayable(music.smallCrash), music.PlaybackMode.InBackground)
+        music.setVolume(12)
+        blockX += -1
+        music.play(music.createSong(assets.song`Music`), music.PlaybackMode.LoopingInBackground)
+        loadLevel()
     }
 })
 function breakBlock () {
@@ -95,6 +136,8 @@ function breakBlock () {
         if (blockListX[index] == blockX && blockListY[index] == blockY) {
             if (blockListType[index] == 1) {
                 coins += 1
+            } else if (blockListType[index] == 2) {
+                coins += 2
             }
             blockListX.removeAt(index)
             blockListY.removeAt(index)
@@ -103,6 +146,7 @@ function breakBlock () {
         }
     }
     tiles.setTileAt(tiles.getTileLocation(blockX, blockY), assets.tile`transparency16`)
+    tiles.setWallAt(tiles.getTileLocation(blockX, blockY), false)
     updateBlockSelect()
 }
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
@@ -112,8 +156,10 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
         if (blockX == playerGridX && blockY == playerGridY) {
             start()
         } else if (selectedBlock == 0) {
-            if (tiles.tileAtLocationEquals(tiles.getTileLocation(blockX, blockY), assets.tile`stone`)) {
-                breakBlock()
+            for (let index = 0; index <= blockListType.length; index++) {
+                if (blockX == blockListX[index] && blockY == blockListY[index]) {
+                    breakBlock()
+                }
             }
         } else {
             if (blockAvailable == 1) {
@@ -125,7 +171,7 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
         }
     } else if (state == 2) {
         // Buy stone block
-        // dx 
+        // 
         if (shopMenuSelect == 0) {
             if (coins >= 1) {
                 music.setVolume(51)
@@ -139,7 +185,22 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
                 rob.sayText("Too expensive!", 1000, false)
             }
         } else {
-        	
+            // Buy bomb
+            if (shopMenuSelect == 2) {
+                if (coins >= 2) {
+                    music.setVolume(51)
+                    music.play(music.melodyPlayable(music.baDing), music.PlaybackMode.InBackground)
+                    coins += -2
+                    selectedBlock = 2
+                    loadLevel()
+                } else {
+                    music.setVolume(75)
+                    music.play(music.melodyPlayable(music.buzzer), music.PlaybackMode.InBackground)
+                    rob.sayText("Too expensive!", 1000, false)
+                }
+            } else {
+            	
+            }
         }
     }
 })
@@ -152,21 +213,33 @@ controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
         }
         grid.place(blockSelect, tiles.getTileLocation(blockX, blockY))
         updateBlockSelect()
+    } else if (state == 2) {
+        if (shopMenuSelect - 1 == 1) {
+            shopMenuSelect = 0
+            refreshShop()
+        } else if (shopMenuSelect - 1 >= 0) {
+            shopMenuSelect = shopMenuSelect - 1
+            refreshShop()
+        } else {
+            music.setVolume(255)
+            music.play(music.melodyPlayable(music.footstep), music.PlaybackMode.InBackground)
+        }
     }
 })
 function loadLevel () {
     state = 1
     rob = sprites.create(assets.image`rob`, SpriteKind.Player)
     blockSelect = sprites.create(assets.image`blockSelect0`, SpriteKind.Block)
+    tiles.setCurrentTilemap(map)
     updateBlockSelect()
     sprites.destroyAllSpritesOfKind(SpriteKind.image)
-    tiles.setCurrentTilemap(tilemap`level1`)
+    sprites.destroyAllSpritesOfKind(SpriteKind.Bomb)
     for (let index = 0; index <= list.length; index++) {
         if (blockListType[index] == 1) {
             tiles.setTileAt(tiles.getTileLocation(blockListX[index], blockListY[index]), assets.tile`stone`)
             tiles.setWallAt(tiles.getTileLocation(blockListX[index], blockListY[index]), true)
-        } else {
-        	
+        } else if (blockListType[index] == 2) {
+            tiles.setTileAt(tiles.getTileLocation(blockListX[index], blockListY[index]), assets.tile`bomb`)
         }
     }
     scene.setBackgroundImage(assets.image`bg`)
@@ -177,20 +250,16 @@ function loadLevel () {
 }
 function blockSelectMenu () {
     state = 2
-    rob.setPosition(96, 22)
+    rob.setPosition(92, 22)
     sprites.destroy(blockSelect)
     tiles.setCurrentTilemap(tilemap`empty`)
     scene.centerCameraAt(0, 0)
     aButton = sprites.create(assets.image`aButton`, SpriteKind.image)
+    shopMenuSelect = 0
     refreshShop()
-    if (coins == 0) {
-        number = sprites.create(assets.image`0`, SpriteKind.image)
-    } else if (coins == 1) {
-        number = sprites.create(assets.image`1`, SpriteKind.image)
-    } else if (coins == 2) {
-        number = sprites.create(assets.image`2`, SpriteKind.image)
-    }
-    number.setPosition(123, 17)
+    number.setPosition(119, 16)
+    number.setText(convertToText(coins))
+    scaling.scaleToPercent(number, 325, ScaleDirection.Uniformly, ScaleAnchor.Middle)
     scaling.scaleToPercent(aButton, 70, ScaleDirection.Uniformly, ScaleAnchor.Middle)
     animation.runImageAnimation(
     aButton,
@@ -201,7 +270,9 @@ function blockSelectMenu () {
 }
 function updateBlockSelect () {
     blockSelect.sayText("")
-    if (tiles.tileAtLocationIsWall(tiles.getTileLocation(blockX, blockY)) || tiles.tileAtLocationEquals(tiles.getTileLocation(blockX, blockY), assets.tile`lava`) || (tiles.tileAtLocationEquals(tiles.getTileLocation(blockX, blockY), assets.tile`flag`) || false)) {
+    if (tiles.tileAtLocationIsWall(tiles.getTileLocation(blockX, blockY)) || tiles.tileAtLocationEquals(tiles.getTileLocation(blockX, blockY), assets.tile`lava`) || (tiles.tileAtLocationEquals(tiles.getTileLocation(blockX, blockY), assets.tile`flag`) || tiles.tileAtLocationEquals(tiles.getTileLocation(blockX, blockY), assets.tile`bomb`))) {
+        blockAvailable = 0
+    } else if (selectedBlock == 2 && !(tiles.tileAtLocationIsWall(tiles.getTileLocation(blockX, blockY + 1)))) {
         blockAvailable = 0
     } else {
         blockAvailable = 1
@@ -211,16 +282,23 @@ function updateBlockSelect () {
         blockSelect.sayText("Go!")
     } else {
         if (selectedBlock == 0) {
-            if (tiles.tileAtLocationEquals(tiles.getTileLocation(blockX, blockY), assets.tile`stone`)) {
-                blockSelect.setImage(assets.image`blockSelect0X`)
-            } else {
-                blockSelect.setImage(assets.image`blockSelect0`)
+            blockSelect.setImage(assets.image`blockSelect0`)
+            for (let index = 0; index <= blockListType.length; index++) {
+                if (blockX == blockListX[index] && blockY == blockListY[index]) {
+                    blockSelect.setImage(assets.image`blockSelect0X`)
+                }
             }
         } else if (selectedBlock == 1) {
             if (blockAvailable == 1) {
                 blockSelect.setImage(assets.image`blockSelect1`)
             } else {
                 blockSelect.setImage(assets.image`blockSelect1X`)
+            }
+        } else if (selectedBlock == 2) {
+            if (blockAvailable == 1) {
+                blockSelect.setImage(assets.image`blockSelect2`)
+            } else {
+                blockSelect.setImage(assets.image`blockSelect2X`)
             }
         }
     }
@@ -234,6 +312,22 @@ controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
         }
         grid.place(blockSelect, tiles.getTileLocation(blockX, blockY))
         updateBlockSelect()
+    } else if (state == 2) {
+        if (itemsUnlocked > 1) {
+            if (shopMenuSelect + 1 == 1) {
+                shopMenuSelect = 2
+                refreshShop()
+            } else if (shopMenuSelect + 1 <= itemsUnlocked) {
+                shopMenuSelect = shopMenuSelect + 1
+                refreshShop()
+            } else {
+                music.setVolume(255)
+                music.play(music.melodyPlayable(music.footstep), music.PlaybackMode.InBackground)
+            }
+        } else {
+            music.setVolume(255)
+            music.play(music.melodyPlayable(music.footstep), music.PlaybackMode.InBackground)
+        }
     }
 })
 controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
@@ -245,10 +339,20 @@ controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
         }
         grid.place(blockSelect, tiles.getTileLocation(blockX, blockY))
         updateBlockSelect()
+    } else if (state == 2) {
+        if (shopMenuSelect + 2 <= itemsUnlocked) {
+            shopMenuSelect = shopMenuSelect + 2
+            refreshShop()
+        } else {
+            music.setVolume(255)
+            music.play(music.melodyPlayable(music.footstep), music.PlaybackMode.InBackground)
+        }
     }
 })
 function cutscene () {
     music.stopAllSounds()
+    music.setVolume(100)
+    music.play(music.melodyPlayable(music.powerDown), music.PlaybackMode.InBackground)
     scene.cameraFollowSprite(null)
     tiles.setCurrentTilemap(tilemap`empty`)
     scene.setBackgroundImage(assets.image`transition`)
@@ -275,6 +379,8 @@ function cutscene () {
         scene.centerCameraAt(142, 200)
         tiles.setCurrentTilemap(tilemap`empty`)
         scene.setBackgroundImage(assets.image`transition`)
+        music.setVolume(100)
+        music.play(music.melodyPlayable(music.powerUp), music.PlaybackMode.InBackground)
         pause(2000)
         transitionText.setText("")
         setupLevel()
@@ -283,13 +389,51 @@ function cutscene () {
 }
 function refreshShop () {
     if (itemsUnlocked == 1) {
+        scene.setBackgroundImage(assets.image`blockSelectScreen1a`)
+        aButton.setPosition(67, 18)
+    } else if (itemsUnlocked == 2) {
         if (shopMenuSelect == 0) {
-            scene.setBackgroundImage(assets.image`blockSelectScreen1a`)
+            scene.setBackgroundImage(assets.image`blockSelectScreen2a`)
             aButton.setPosition(67, 18)
-        } else {
-        	
+        } else if (shopMenuSelect == 2) {
+            scene.setBackgroundImage(assets.image`blockSelectScreen2b`)
+            aButton.setPosition(67, 49)
         }
     }
+}
+function explodeBombs () {
+    for (let index = 0; index <= blockListType.length; index++) {
+        if (blockListType[index] == 2) {
+            tiles.setTileAt(tiles.getTileLocation(blockListX[index], blockListY[index]), assets.tile`transparency16`)
+            bomb = sprites.create(assets.image`bomb3`, SpriteKind.Bomb)
+            tiles.placeOnTile(bomb, tiles.getTileLocation(blockListX[index], blockListY[index]))
+            animation.runImageAnimation(
+            bomb,
+            assets.animation`bombAnim1`,
+            1000,
+            false
+            )
+        }
+    }
+    timer.after(3000, function () {
+        if (state == 3) {
+            sprites.destroyAllSpritesOfKind(SpriteKind.Bomb)
+            for (let index = 0; index <= blockListType.length; index++) {
+                if (blockListType[index] == 2) {
+                    music.setVolume(232)
+                    music.play(music.melodyPlayable(music.bigCrash), music.PlaybackMode.InBackground)
+                    bomb = sprites.create(assets.image`bomb3`, SpriteKind.Bomb)
+                    tiles.placeOnTile(bomb, tiles.getTileLocation(blockListX[index], blockListY[index]))
+                    animation.runImageAnimation(
+                    bomb,
+                    assets.animation`bombAnim2`,
+                    25,
+                    false
+                    )
+                }
+            }
+        }
+    })
 }
 function start () {
     music.stopAllSounds()
@@ -300,13 +444,16 @@ function start () {
     scene.cameraFollowSprite(rob)
     rob.ay = 500
     rob.vx = 50
+    rob.z = 5
+    explodeBombs()
 }
-let number: Sprite = null
+let bomb: Sprite = null
 let aButton: Sprite = null
+let blockAvailable = 0
 let rob: Sprite = null
 let shopMenuSelect = 0
-let blockAvailable = 0
 let blockSelect: Sprite = null
+let map: tiles.TileMapData = null
 let itemsUnlocked = 0
 let coins = 0
 let maxBlockX = 0
@@ -322,11 +469,11 @@ let blockListType: number[] = []
 let blockListY: number[] = []
 let blockListX: number[] = []
 let selectedBlock = 0
+let number: TextSprite = null
 let transitionText: TextSprite = null
 let titleText: TextSprite = null
 let state = 0
 state = 0
-let textSprite = 0
 scene.setBackgroundImage(assets.image`mainMenuScreen`)
 music.play(music.createSong(assets.song`menuSong`), music.PlaybackMode.LoopingInBackground)
 titleText = textsprite.create("Rob's Lab")
@@ -334,6 +481,7 @@ scaling.scaleToPercent(titleText, 200, ScaleDirection.Uniformly, ScaleAnchor.Mid
 titleText.setPosition(81, 16)
 transitionText = textsprite.create("Press any button to start")
 transitionText.setPosition(81, 111)
+number = textsprite.create("", 0, 15)
 game.onUpdate(function () {
     if (state == 3) {
         if (rob.isHittingTile(CollisionDirection.Right)) {
@@ -358,6 +506,7 @@ game.onUpdate(function () {
             loadLevel()
         }
         if (tiles.tileAtLocationEquals(rob.tilemapLocation(), assets.tile`flag`)) {
+            sprites.destroyAllSpritesOfKind(SpriteKind.Bomb)
             rob.setVelocity(rob.vx / 20, 0)
             state = 4
             effects.confetti.startScreenEffect(1000)
